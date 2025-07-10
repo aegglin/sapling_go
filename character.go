67c1e69b44ba9df5e4c0b5ee2c3e3c8ae2f6ca33
Aiden Egglin <uefadude@gmail.com>
2025-07-10 12:27:59 -0400

    Initial commit for type hierarchy

---
+++ character.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// the character has x, y, vx, and vy
+type character struct {
+	x                          int
+	y                          int
+	speed                      int
+	direction                  Direction
+	currentSprite              *ebiten.Image
+	currentSpriteNumber        int
+	spriteUpdateFrameCount     int
+	spriteFrameSwitchThreshold int
+}
