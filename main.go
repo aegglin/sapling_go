8f33b5b88e69181b0d5885810f8263185f4876dd
Aiden Egglin <uefadude@gmail.com>
2025-07-16 12:42:40 -0400

    Add tile drawing

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	_ "image/png"
 	"log"
@@ -64,11 +63,9 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// ebitenutil.DebugPrint(screen, "Hello, World!")
-	fmt.Println("Starting drawing")
 	screen.Fill(color.White)
 	g.mapTileHandler.DrawAll(screen)
 	g.beetle.Draw(screen)
-	fmt.Println("Done drawing")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -80,14 +77,9 @@ func main() {
 	ebiten.SetWindowSize(gameWidth, gameHeight)
 	ebiten.SetWindowTitle("Sapling by Aiden Egglin")
 
-	fmt.Println("creating map handler")
 	mapTileHandler := &MapTileHandler{}
 	mapTileHandler.LoadMap()
 	mapTileHandler.LoadTileImages()
-	fmt.Println("Done creating map handler")
-
-	// fmt.Println(mapTileHandler.mapTileNumbers)
-	// fmt.Println(mapTileHandler.mapTiles)
 
 	beetle := Beetle{character{x: 50, y: 50, direction: Up, speed: 4, currentSpriteNumber: 1, currentSprite: leftSprite1, spriteFrameSwitchThreshold: 12}}
 	beetle.LoadImages()
--- maptilehandler.go
+++ maptilehandler.go
@@ -13,7 +13,7 @@ import (
 
 type MapTileHandler struct {
 	mapTileNumbers [numWorldRows][numWorldColumns]int
-	mapTiles       [11]MapTile
+	mapTiles       [11]*MapTile
 }
 
 func (mapTileHandler *MapTileHandler) LoadMap() {
@@ -46,77 +46,77 @@ func (mapTileHandler *MapTileHandler) LoadTileImages() {
 		log.Fatal(err)
 	}
 	grassTile := MapTile{Image: grass, IsSolid: false}
-	mapTileHandler.mapTiles[0] = grassTile
+	mapTileHandler.mapTiles[0] = &grassTile
 
 	tree1, _, err = ebitenutil.NewImageFromFile("assets/tiles/Tree1.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	tree1Tile := MapTile{Image: tree1, IsSolid: true}
-	mapTileHandler.mapTiles[1] = tree1Tile
+	mapTileHandler.mapTiles[1] = &tree1Tile
 
 	tree2, _, err = ebitenutil.NewImageFromFile("assets/tiles/Tree2.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	tree2Tile := MapTile{Image: tree2, IsSolid: true}
-	mapTileHandler.mapTiles[2] = tree2Tile
+	mapTileHandler.mapTiles[2] = &tree2Tile
 
 	tree3, _, err = ebitenutil.NewImageFromFile("assets/tiles/Tree3.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	tree3Tile := MapTile{Image: tree3, IsSolid: true}
-	mapTileHandler.mapTiles[3] = tree3Tile
+	mapTileHandler.mapTiles[3] = &tree3Tile
 
 	shrub, _, err = ebitenutil.NewImageFromFile("assets/tiles/Shrub.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	shrubTile := MapTile{Image: shrub, IsSolid: true}
-	mapTileHandler.mapTiles[4] = shrubTile
+	mapTileHandler.mapTiles[4] = &shrubTile
 
 	underbrush, _, err = ebitenutil.NewImageFromFile("assets/tiles/Underbrush.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	underbrushTile := MapTile{Image: underbrush, IsSolid: false}
-	mapTileHandler.mapTiles[5] = underbrushTile
+	mapTileHandler.mapTiles[5] = &underbrushTile
 
 	shrubUnderbrush, _, err = ebitenutil.NewImageFromFile("assets/tiles/Shrub_Underbrush.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	shrubUnderbrushTile := MapTile{Image: shrubUnderbrush, IsSolid: false}
-	mapTileHandler.mapTiles[6] = shrubUnderbrushTile
+	mapTileHandler.mapTiles[6] = &shrubUnderbrushTile
 
 	orangeFlower, _, err = ebitenutil.NewImageFromFile("assets/tiles/OrangeFlower.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	orangeFlowerTile := MapTile{Image: orangeFlower, IsSolid: false}
-	mapTileHandler.mapTiles[7] = orangeFlowerTile
+	mapTileHandler.mapTiles[7] = &orangeFlowerTile
 
 	treeFlies1, _, err = ebitenutil.NewImageFromFile("assets/tiles/Tree1_Flies1.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	treeFliesTile := MapTile{Image: treeFlies1, IsSolid: true}
-	mapTileHandler.mapTiles[8] = treeFliesTile
+	mapTileHandler.mapTiles[8] = &treeFliesTile
 
 	treeBeehive1, _, err = ebitenutil.NewImageFromFile("assets/tiles/Tree1_Beehive1.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	treeBeehiveTile := MapTile{Image: treeBeehive1, IsSolid: true}
-	mapTileHandler.mapTiles[9] = treeBeehiveTile
+	mapTileHandler.mapTiles[9] = &treeBeehiveTile
 
 	treeWoodpecker1, _, err = ebitenutil.NewImageFromFile("assets/tiles/Tree1_Woodpecker1.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	treeWoodpeckerTile := MapTile{Image: treeWoodpecker1, IsSolid: true}
-	mapTileHandler.mapTiles[10] = treeWoodpeckerTile
+	mapTileHandler.mapTiles[10] = &treeWoodpeckerTile
 }
 
 func (mapTileHandler *MapTileHandler) DrawAll(screen *ebiten.Image) {
@@ -124,17 +124,24 @@ func (mapTileHandler *MapTileHandler) DrawAll(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(imageScale, imageScale)
 
+	var mapTileNumber int
+	var image *ebiten.Image
+
 	for row := range numWorldRows {
 		for col := range numWorldColumns {
-			// fmt.Printf("row: %d, column: %d", row, col)
-			fmt.Printf("%f, %f\n", float64(row)*tileSize, float64(col)*tileSize)
-			op.GeoM.Translate(float64(row)*tileSize, float64(col)*tileSize)
 
-			mapTileNumber := mapTileHandler.mapTileNumbers[row][col]
-			// fmt.Println("mapTileNumber: ", mapTileNumber)
-			image := mapTileHandler.mapTiles[mapTileNumber]
-			// fmt.Println("image: ", image)
-			screen.DrawImage(image.Image, op)
+			// GeoM.Translate is relative to the current position
+			if row == 0 && col == 0 {
+				op.GeoM.Translate(0, 0)
+			} else if col == numWorldColumns-1 {
+				op.GeoM.Translate(-(numWorldRows-1)*tileSize, tileSize)
+			} else {
+				op.GeoM.Translate(tileSize, 0)
+			}
+
+			mapTileNumber = mapTileHandler.mapTileNumbers[row][col]
+			image = mapTileHandler.mapTiles[mapTileNumber].Image
+			screen.DrawImage(image, op)
 		}
 	}
 }
